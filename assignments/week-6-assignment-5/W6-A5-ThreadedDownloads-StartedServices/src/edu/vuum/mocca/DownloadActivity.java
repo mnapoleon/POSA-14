ba56bff587407571c7259f9646e6bf7279ac6c96
Michael Napoleon <michael.napoleon@gmail.com>
2014-06-18 10:26:52 -0400

    finished week 6 assignment

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -2,6 +2,8 @@ package edu.vuum.mocca;
 
 import java.lang.ref.WeakReference;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
@@ -78,6 +80,10 @@ public class DownloadActivity extends DownloadBase {
                 // bitmap that's been downloaded and returned to
                 // the DownloadActivity as a pathname who's Bundle
             	// key is defined by DownloadUtils.PATHNAME_KEY
+            	Bundle data = msg.getData();
+            	String imagePath = data.getString("PATHNAME");
+            	
+            	activity.displayBitmap(imagePath);
             }
     	}
     }
@@ -107,7 +113,8 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // DownloadIntentService with the appropriate Intent
             // returned from the makeIntent() factory method.
-
+        	Intent downloadServiceIntent = DownloadIntentService.makeIntent(getApplicationContext(), handler, getUrlString());
+        	startService(downloadServiceIntent);
             which = "Starting IntentService";
             break;
         
@@ -115,7 +122,8 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // ThreadPoolDownloadService with the appropriate Intent
             // returned from the makeIntent() factory method.
-
+        	Intent threadPoolIntent = ThreadPoolDownloadService.makeIntent(getApplicationContext(), handler, getUrlString());
+        	startService(threadPoolIntent);
             which = "Starting ThreadPoolDownloadService";
             break;
         
